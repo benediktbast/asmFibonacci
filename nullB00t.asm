85d719216cebe0d2860ad09c25d49581b337c8e7
Benedikt Bast <mail@benediktbast.com>
2017-11-25 14:54:28 +0100

    routines for reset video position and clear screen

--- nullB00t.asm
+++ nullB00t.asm
@@ -1,36 +1,69 @@
-[BITS 16]			; 16 bit code
-[ORG 0x7C00]			; load  code to memory 0x7C00
+[BITS 16]				; 16 bit code
+[ORG 0x7C00]				; load  code to memory 0x7C00
 
-MOV SI, WelcomeMsg 		; store string pointer to SI
-CALL PrintString		; call print string procedure
-JMP $				; infinite loop
+_start:
+	call _clearScreen		; redraw screen
+
+	mov si, WelcomeMsg 		; store string pointer to SI
+	call _printString		; call print string procedure
+
+	jmp $				; just an infinite loop
+
+; procedure to move video outpout to a given poisition
+; input : dl = x position
+; input : dh = y poisition
+_setVideoPosition:
+	mov ah, 0x2			; move position in video buffer
+	int 0x10			; call video interrupt
+	ret
+
+; procedure to move video output to position 0,0
+_resetVideoPosition:
+	mov dl, 0x0			; set x = 0
+	mov dh, 0x0			; set y = 0
+	call _setVideoPosition		; set position
+	ret
+
+; procedure to redraw screen in blue color
+_clearScreen:
+	call _resetVideoPosition	; reset cursor to 0,0
+	mov ah, 0x9			; number of bytes defined in cx
+	mov cx, 0x1000			; 4096 bytes
+	mov al, 0x20			; white space
+	mov bl, 0x17			; text attribute blue
+	int 0x10			; call video interrupt
+	ret
 
 
 ; procedure to print one char to the string
-; input: Ascii value in SI
-PrintCharacter:
-	MOV AH, 0x0E		; print char
-	MOV BH, 0x00		; page no
-	MOV BL, 0x07		; text attribute lightgrey font on black
-	INT 0x10		; call video interrupt
-	RET			; exit
+; input: Ascii value in AL
+_printCharacter:
+	mov ah, 0x0E		; print one char 
+	mov bh, 0x00		; page no
+	mov bl, 0x07		; text attribute lightgrey font on black
+	int 0x10		; call video interrupt
+	ret			; exit
+
 
 ;Procedure to Print a null terminated String on screen
 ;input: char pointer in SI
-PrintString:
-next_char:	
+_printString:
+
+.nextChar:
 	MOV AL, [SI]		; store ascii value from SI to AL
 	INC SI			; increment pointer
 	OR AL, AL		; if AL contains null
-	JZ exit_function 	; exit
-	CALL PrintCharacter 	; print AL to screen
-	JMP next_char	; next char
-exit_function:
+	JZ .exitPrintString 	; exit
+	CALL _printCharacter 	; print AL to screen
+	JMP .nextChar		; next char
+
+.exitPrintString:
 	RET			; exit
 
 
-;Data
+; data
 WelcomeMsg db 'Hello World', 0
+
 
 TIMES 510 - ($ - $$) db 0	; fill up with 0 to reach 512KB
 DW 0xAA55			; boot signature at the

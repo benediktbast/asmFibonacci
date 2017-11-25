b76d021576aa89668340633304c9cc600424271d
Benedikt Bast <mail@benediktbast.com>
2017-11-25 12:08:14 +0100

    add simple bootloader

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
-BINARIES = fibonacci
+BINARIES = fibonacci nullB00t.bin
 ASM = nasm
 ASM_FLAGS = -f elf64
 LINKER = ld
+QEMU = qemu-system-x86_64
 
 all: $(BINARIES)
+
+nullB00t:
+	$(ASM) nullB00t.asm -f bin -o nullB00t.bin
+
+nullB00tQemu: clean nullB00t
+	$(QEMU) -m 16 nullB00t.bin  
 
 %.o : %.asm
 	$(ASM) $(ASM_FLAGS) -o $@ $<
@@ -14,6 +21,4 @@ all: $(BINARIES)
 .PHONY: clean
 
 clean:
-	$(RM) *.o *.swp ~*
-
-
+	$(RM) *.o *.swp ~* $(BINARIES)
---
+++ nullB00t.asm
@@ -0,0 +1,36 @@
+[BITS 16]			; 16 bit code
+[ORG 0x7C00]			; load  code to memory 0x7C00
+
+MOV SI, WelcomeMsg 		; store string pointer to SI
+CALL PrintString		; call print string procedure
+JMP $				; infinite loop;
+
+
+; procedure to print one char to the string
+; input: Ascii value in SI
+PrintCharacter:
+	MOV AH, 0x0E		;Print char
+	MOV BH, 0x00		;Page no
+	MOV BL, 0x07		;Text attribute lightgrey font on black
+	INT 0x10		;Video interrupt
+	RET			;Return to calling procedure
+
+;Procedure to Print a null terminated String on screen
+;input: char pointer in SI
+PrintString:
+next_char:	
+	MOV AL, [SI]		; store ascii value from SI to AL
+	INC SI			; increment pointer
+	OR AL, AL		; if AL contains null
+	JZ exit_function 	; exit
+	CALL PrintCharacter 	; print AL to screen
+	JMP next_char	; next char
+exit_function:
+	RET			; exit
+
+
+;Data
+WelcomeMsg db 'Hello World', 0		;
+
+TIMES 510 - ($ - $$) db 0	; fill up with 0 to reach 512KB
+DW 0xAA55			; boot signature at the

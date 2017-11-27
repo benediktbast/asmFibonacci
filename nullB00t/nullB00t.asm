8c94ca79e05f4f63220c6752d3d51b58d6f5905d
Benedikt Bast <mail@benediktbast.com>
2017-11-27 21:16:17 +0100

    use hex value for stack segment calculation

--- nullB00t/nullB00t.asm
+++ nullB00t/nullB00t.asm
@@ -4,7 +4,7 @@
 _nullBootMain:
 
 	mov ax, 0x7C00			; 4K stack space after the bootloader
-	add ax, 288			; 4096b + 512b (bl size) / 16b per frame
+	add ax, 0x120			; 4096b + 512b (bl size) / 16b per frame
 	mov ss, ax			; set stack space
 	mov sp, 0x1000			; set stack pointer to offset 4096
 

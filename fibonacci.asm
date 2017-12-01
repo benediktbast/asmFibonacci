94836ff7844ab41455c2f8b66e528aa0286379c5
Benedikt Bast <mail@benediktbast.com>
2017-12-01 20:43:34 +0100

    use new print functions, use colored output via vt100 sequences

--- fibonacci.asm
+++ fibonacci.asm
@@ -2,20 +2,21 @@
 %include 'print-linux-x86_64.asm'	; include sprint, iprint, etc ...
 
 section .data
-	msg db "How many numbers do you want to add? ",0
-	ofErrorMsg db "<< Overflow Exit program >>",10,0
-	exitMsg db "- Done -",10,0
-	defaultMax equ 20
+	msg db 'How many numbers do you want to add? ',0
+	ofErrorMsg db '<< Overflow Exit program >> ',0
+	exitMsg db "- Done -",0
+	fontRed db 0x1b,'[31m',0
+	fontGreen db 0x1b,'[32m',0
+	fontReset db 0x1b,'[0m',0
+	defaultMax equ 24
 	sys_exit equ 60
-	sys_write equ 1
+;	sys_write equ 1
 	sys_read equ 0
-	std_out equ 1
+;	std_out equ 1
 	std_in equ 0
 	
 section .bss
 	inputBuffer resb 64		; user input for maximum number
-	strBuffer resb 64		; 100 bytes for integer conversion
-	strBufferPos resb 8		; 8 bytes for array position
 
 section .text
 	global _start
@@ -31,11 +32,17 @@ _start:
 	call _sprint
 
 	mov rax, defaultMax		; print default max
-	call _printInt
+	call _iprintln
 
 	call _fibonacci			; calculate fibonacci
 
+	mov rax, fontGreen
+	call _sprint
+
 	mov rax, exitMsg
+	call _sprintln
+
+	mov rax, fontReset
 	call _sprint
 
 	exit 0				; call macro exit(0)
@@ -44,7 +51,7 @@ _start:
 _fibonacci:
 	mov rax,  1			; param 2
 	mov rdx,  0			; param 1
-	mov rbx, defaultMax		; set counter to max, decrement later
+	mov rcx, defaultMax		; set counter to max, decrement later
 
 .fibonacciLoop:
 	push rax			; save param 2 on stack
@@ -53,10 +60,10 @@ _fibonacci:
 	jo _exitOF			; exit when overflow happens
 
 	push rax			; save rax
-	call _printInt			; print result from rax
+	call _iprintln			; print result from rax
 	pop rax				; restore rax
 
-	sub rbx, 1			; decrement counter
+	sub rcx, 1			; decrement counter
 	jnz .fibonacciLoop		; countinue loop if rbx > 0 
 
 	ret				; else return
@@ -71,60 +78,14 @@ _getMaxNum:
 	syscall				; sys_read(std_in, inputBuffer, 8)
 	ret
 
-; input: rax = integer to print
-; output: print integers as string to std_out
-_printInt:
-	push rbx
-	push rcx
-	push rdx
-
-	mov rcx, strBuffer		; move adress of buffer to rcx
-	mov rbx, 10			; new line character
-	mov [rcx], rbx			; add new line character first position of array
-	inc rcx				; increment adress after adding cr
-	mov [strBufferPos], rcx		; set array position to second element in strBuffer
-
-; convert ints to char
-; store to strBuffer in reversed order
-.printIntLoop:
-	xor rdx, rdx			; set rdx (remainder) to zero before division
-	mov rbx, 10			; move divisor to rbx
-	div rbx				; divide rax by 10
-	push rax			; push result to stack
-	add rdx, 48			; remainder + 48 = ascii code for one integer
-	
-	mov rcx, [strBufferPos]		; current position in array
-	mov [rcx], dl			; move char (lower 8 bytes of rdx) to array
-	inc rcx				; increment position
-	mov [strBufferPos], rcx		; update pointer to array Position
-
-	pop rax				; pop division result from stack
-	or rax, rax			; if division result !=0
-	jnz .printIntLoop		; continue loop
-
-; print chars in strBuffer in corret order to std
-.printIntLoop2: 
-	mov rcx, [strBufferPos]		; move last char to rcx
-
-	mov rax, sys_write
-	mov rdi, std_out
-	mov rsi, rcx			; pointer to last char
-	mov rdx, 1			; 1 byte
-	syscall
-
-	mov rcx, [strBufferPos]
-	dec rcx				; decrement array pointer
-	mov [strBufferPos], rcx		; update array pointer
-
-	cmp rcx, strBuffer		; if array position >= start of int bffer
-	jge .printIntLoop2		; continue printing
-
-	pop rdx
-	pop rcx
-	pop rbx
-	ret
-
 _exitOF:
+	mov rax, fontRed
+	call _sprint
+
 	mov rax, ofErrorMsg
-	call _sprint		; print error message
+	call _sprintln			; print error message
+
+	mov rax, fontReset
+	call _sprint
+
 	exit 1				; call macro exit(1)
